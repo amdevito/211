cdde3954001d9a330b685d7905622a590f991ddc
amdevito <amdevitoam@gmail.com>
2020-11-26 15:17:16 -0500

    Final_Project: Added hover direction text to direct user to uncover the patron's answer. Next, do not allow user's answers to be unvailed until after the parton's response is uncovered. Old answer is visable first.

--- Final_Project/narrative/js/script.js
+++ Final_Project/narrative/js/script.js
@@ -1,12 +1,21 @@
-let video = document.querySelector("#videoElement");
+window.onload = function () {
+  let video = document.querySelector("#videoElement");
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
-}
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+
+  let hoverText = document.getElementById("hover_text");
+
+  hoverText.addEventListener("mouseover", function () {
+    document.getElementById("overlay_text").style.opacity = 1;
+    hoverText.style.opacity = 0;
+  });
+};
