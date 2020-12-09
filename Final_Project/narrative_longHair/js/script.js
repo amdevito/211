f2bc218ac5c677aae43f06cea62fb75d395cf118
amdevito <amdevitoam@gmail.com>
2020-12-09 14:28:37 -0500

    testing tab close

--- Final_Project/narrative_longHair/js/script.js
+++ Final_Project/narrative_longHair/js/script.js
@@ -18,4 +18,12 @@ window.onload = function () {
     document.getElementById("overlay_text").style.opacity = 1;
     hoverText.style.opacity = 0;
   });
+
+  document.getElementById("testLink").addEventListener("click", closeWin);
+
+  function closeWin() {
+    console.log("closeWindow");
+    // window.close();
+    open(location, "_self").close();
+  }
 };
